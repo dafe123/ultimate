4a6f55256cf7955f7bcc4a1ad79cdba0c8b94ece
Jill Enke <jill.enke@gmail.com>
2019-11-20 18:07:43 +0100

    fix location of bitvec procedure and add it to convertIfNecessary
    Also includes some fixes to faulty Types in the procedure call &
    restructuring of convertIfNecessary to allow easier addition of a
    procedure for floating type results.

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/CHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/CHandler.java
@@ -4068,7 +4068,7 @@ public class CHandler {
 			final CallStatement call = StatementFactory.constructCallStatement(
 					loc,
 					false, new VariableLHS[] {auxvarinfo.getLhs()},
-					"float_to_bitvec32",
+					"float_to_bitvec"  + Integer.toString(mTypeSizes.getFloatingPointSize(cType).getBitSize()),
 					arguments);
 			builder.addStatement(call);
 			builder.setLrValue(new RValue(auxvarinfo.getExp(), new CPrimitive(CPrimitives.FLOAT)));
@@ -4462,7 +4462,7 @@ public class CHandler {
 			final CallStatement call = StatementFactory.constructCallStatement(
 					loc,
 					false, new VariableLHS[] {auxvarinfo.getLhs()},
-					"float_to_bitvec32",
+					"float_to_bitvec"  + Integer.toString(mTypeSizes.getFloatingPointSize(cType).getBitSize()),
 					arguments);
 			result.addStatement(call);
 			result.setLrValue(new RValue(auxvarinfo.getExp(), new CPrimitive(CPrimitives.FLOAT)));
